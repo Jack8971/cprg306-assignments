3cb6c286f246f76cdd6be6acab1a2ef3b5d1df64
Jack <Jackson.battista02@gmail.com>
2025-10-05 11:41:40 -0600

    week 5 page complete

--- src/app/week-5/page.js
+++ src/app/week-5/page.js
@@ -5,7 +5,7 @@ export default function Week4Page() {
   return (
     <main className="min-h-screen bg-black p-6">
       <div className="w-96 mx-auto text-left">
-        <h1 className="text-2xl font-bold mb-4 text-white">Add New Item</h1>
+        <h1 className="text-2xl font-bold mb-4 text-white">Week 5 - New Item</h1>
         <NewItem />
       </div>
     </main>
