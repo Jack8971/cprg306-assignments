e7e4a7936fcaa544701eabfc0d63473126d290d6
Jack <Jackson.battista02@gmail.com>
2025-10-06 12:53:20 -0600

    Addition of week 6

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,21 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main style={{ backgroundColor: 'black', minHeight: '100vh' }}>
+    <main style={{ backgroundColor: 'black', minHeight: '100vh' }}> {/*add some style to the assignment. Dont knwo excactly what styling to it*/}
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
+        {/* Assignment links for all weeks */}
         <Link href="/week-2">Go to Week 2 ➜ </Link>
         <Link href="/week-3">Go to Week 3 ➜ </Link>
         <Link href="/week-4">Go to Week 4 ➜ </Link>
         <Link href="/week-5">Go to Week 5 ➜ </Link>
+        <Link href="/week-6">Go to Week 6 ➜ </Link>
+        {/* <Link href="/week-7">Go to Week 7 ➜ </Link>
+        <Link href="/week-8">Go to Week 8 ➜ </Link>
+        <Link href="/week-9">Go to Week 9 ➜ </Link>
+        <Link href="/week-10">Go to Week 10 ➜ </Link> */}
+
+
       </p>
     </main>
   );
