f230ec30934c789de41a146a38ed287fddd798da
Jack <Jackson.battista02@gmail.com>
2025-11-17 18:00:39 -0700

    helped

--- src/app/week-9/_utils/firebase.js
+++ src/app/week-9/_utils/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// week-9/_utils/firebase.js
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCH1aIGhQcS9MCCGadu8Oy7QMDK7z9u_l0",
   authDomain: "cprg306-assignments-648a0.firebaseapp.com",
@@ -14,6 +11,7 @@ const firebaseConfig = {
   appId: "1:1038711238777:web:a50f217166d894bea1e4a8"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// ✅ Prevent duplicate initialization
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
