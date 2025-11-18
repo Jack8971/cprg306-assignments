7da3f075b4d628b6f8e9cfbbece6ff35a6f092a2
Jack <Jackson.battista02@gmail.com>
2025-11-17 17:39:40 -0700

    test

--- src/app/week-10/_utils/firebase.js
+++ src/app/week-10/_utils/firebase.js
@@ -1,37 +1,19 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-// import { getAuth } from "firebase/auth";
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCH1aIGhQcS9MCCGadu8Oy7QMDK7z9u_l0",
-//   authDomain: "cprg306-assignments-648a0.firebaseapp.com",
-//   projectId: "cprg306-assignments-648a0",
-//   storageBucket: "cprg306-assignments-648a0.firebasestorage.app",
-//   messagingSenderId: "1038711238777",
-//   appId: "1:1038711238777:web:a50f217166d894bea1e4a8"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// week-10/_utils/firebase.js
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your actual Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCH1aIGhQcS9MCCGadu8Oy7QMDK7z9u_l0",
+  authDomain: "cprg306-assignments-648a0.firebaseapp.com",
+  projectId: "cprg306-assignments-648a0",
+  storageBucket: "cprg306-assignments-648a0.firebasestorage.app",
+  messagingSenderId: "1038711238777",
+  appId: "1:1038711238777:web:a50f217166d894bea1e4a8"
 };
 
-const app = initializeApp(firebaseConfig);
+// Prevent duplicate initialization
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
